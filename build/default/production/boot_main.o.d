bf177752a26c55f968b0a647750321957236e867
EMIC Compiler <emic-compiler@emic.io>
2026-07-20 21:13:33 -0300

    Code generation: 2026-07-21 00:13:33 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: boot
    Project: boot-build
    User: mariano-hunkeler-at-rfindustrial-com

---
+++ build/default/production/boot_main.o.d
@@ -0,0 +1,8 @@
+/root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/build/default/production/boot_main.o: \
+ /root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/boot_main.c \
+ /root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/inc/boot_config.h \
+ /root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/inc/boot_id.h
+
+/root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/inc/boot_config.h:
+
+/root/Dropbox/EMIC/TARGETS/mariano.hunkeler__rfindustrial.com/boot_build/BOOT/inc/boot_id.h:
